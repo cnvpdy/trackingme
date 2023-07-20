b05114b732e35160c87fe01633d93e8e98142783
cnvpdy <101261579+cnvpdy@users.noreply.github.com>
2023-07-21 00:56:41 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
+function guid() {
+    function s4() {
+        return Math
+            .floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() +
+            s4();
+}
 
 function mysqlDb(sql) {
     //mysql
@@ -15,7 +25,7 @@ function mysqlDb(sql) {
         port: '3307',
         user: 'CNV',
         password: 'ausRhdrks2!!',
-        database: 'costdb'
+        database: 'trackingme'
     };
 
     let connection = mysql.createConnection(conn); // DB 커넥션 생성
@@ -28,7 +38,6 @@ function mysqlDb(sql) {
         console.log(results);
         resultArray = results;
     });
-    return resultArray
     connection.end(); // DB 접속 종료
     // mysql
 
@@ -50,35 +59,68 @@ app.get('/write', function (요청, 응답) {
 app.post('/add', function (요청, 응답) {
     응답.send('전송완료');
     console.log(요청.body);
+    let id = guid();
+    let div = 요청.body.div;
+    let cate = 요청.body.cate;
+    let startDate = 요청.body.startDate;
+    let startTime = 요청.body.startTime;
+    let endDate = 요청.body.endDate;
+    let endTime = 요청.body.endTime;
+    let partyPeople = 요청.body.partyPeople;
     let title = 요청.body.title;
-    let date = 요청.body.date;
-    mysqlDb("INSERT todoapp.post VALUES ('" + title + "','" + date + "');");
+    let inputNumber = 요청.body.inputNumber;
+    let inputLocation = 요청.body.inputLocation;
+    let inputTool = 요청.body.inputTool;
+    let inputPurpose = 요청.body.inputPurpose;
+    let inputCost = 요청.body.inputCost;
+    let inputCostBase = 요청.body.inputCostBase;
+
+    let inputReturn = 요청.body.inputReturn;
+    let inputReturnBase = 요청.body.inputReturnBase;
+    let inputRef = 요청.body.inputRef;
+
+    console.log(
+        "INSERT trackingme.raw VALUES ('" + div + "','" + cate + "','" + startDate + "'" +
+        ",'" + startTime + "','" + endDate + "','" + endTime + "','" +
+        partyPeople + "','" + title + "','" + inputNumber + "','" +
+        inputLocation + "','" + inputTool + "','" + inputPurpose + "','" +
+        inputCost + "','" + inputCostBase + "','" + inputReturn + "','" +
+        inputReturnBase + "','" + inputRef + "','" + id + "');"
+    );
+    mysqlDb(
+        "INSERT trackingme.raw VALUES ('" + div + "','" + cate + "','" + startDate + "'" +
+        ",'" + startTime + "','" + endDate + "','" + endTime + "','" +
+        partyPeople + "','" + title + "','" + inputNumber + "','" +
+        inputLocation + "','" + inputTool + "','" + inputPurpose + "','" +
+        inputCost + "','" + inputCostBase + "','" + inputReturn + "','" +
+        inputReturnBase + "','" + inputRef + "','" + id + "');"
+    );
 });
 
-app.get('/list', function(요청, 응답){
-    
+app.get('/list', function (요청, 응답) {
 
-    
     const mysql = require('mysql'); // mysql 모듈 로드
     const conn = { // mysql 접속 설정
         host: '1.221.61.36',
         port: '3307',
         user: 'CNV',
         password: 'ausRhdrks2!!',
-        database: 'costdb'
+        database: 'trackingme'
     };
 
     let connection = mysql.createConnection(conn); // DB 커넥션 생성
     connection.connect(); // DB 접속
-    
-    connection.query("SELECT * FROM todoapp.post;", function (err, results, fields) {
-        if (err) {
-            console.log(err);
+
+    connection.query(
+        "SELECT * FROM trackingme.raw;",
+        function (err, results, fields) {
+            if (err) {
+                console.log(err);
+            }
+            console.log(results);
+            응답.render('list.ejs', {posts: results});
         }
-        console.log(results);
-        응답.render('list.ejs',{posts : results});
-    });
+    );
     connection.end(); // DB 접속 종료
-    
-    
+
 })
