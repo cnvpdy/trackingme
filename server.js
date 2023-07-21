76fd480a172da6ba8b4c7845c38251c04ad3953f
cnvpdy <101261579+cnvpdy@users.noreply.github.com>
2023-07-21 10:49:48 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get('/write', function (요청, 응답) {
 });
 
 app.post('/add', function (요청, 응답) {
-    응답.send('전송완료');
+    응답.sendFile(__dirname + '/process.html');
     console.log(요청.body);
     let id = guid();
     let div = 요청.body.div;
@@ -79,13 +79,25 @@ app.post('/add', function (요청, 응답) {
     let inputReturnBase = 요청.body.inputReturnBase;
     let inputRef = 요청.body.inputRef;
 
+    let item = 요청.body.item;
+    let spec = 요청.body.spec;
+    let unit = 요청.body.unit;
+    let quantity = 요청.body.quantity;
+
+    let perCostMaterial = 요청.body.perCostMaterial;
+    let perCostHuman = 요청.body.perCostHuman;
+    let perCostEtc = 요청.body.perCostEtc;
+    let perCostTotal = 요청.body.perCostTotal;
+
     console.log(
         "INSERT trackingme.raw VALUES ('" + div + "','" + cate + "','" + startDate + "'" +
         ",'" + startTime + "','" + endDate + "','" + endTime + "','" +
         partyPeople + "','" + title + "','" + inputNumber + "','" +
         inputLocation + "','" + inputTool + "','" + inputPurpose + "','" +
         inputCost + "','" + inputCostBase + "','" + inputReturn + "','" +
-        inputReturnBase + "','" + inputRef + "','" + id + "');"
+        inputReturnBase + "','" + inputRef + "','" + id + "','" + item + "','" + spec +
+        "','" + unit + "','" + quantity + "','" + perCostMaterial + "','" +
+        perCostHuman + "','" + perCostEtc + "','" + perCostTotal + "');"
     );
     mysqlDb(
         "INSERT trackingme.raw VALUES ('" + div + "','" + cate + "','" + startDate + "'" +
@@ -93,8 +105,12 @@ app.post('/add', function (요청, 응답) {
         partyPeople + "','" + title + "','" + inputNumber + "','" +
         inputLocation + "','" + inputTool + "','" + inputPurpose + "','" +
         inputCost + "','" + inputCostBase + "','" + inputReturn + "','" +
-        inputReturnBase + "','" + inputRef + "','" + id + "');"
+        inputReturnBase + "','" + inputRef + "','" + id + "','" + item + "','" + spec +
+        "','" + unit + "','" + quantity + "','" + perCostMaterial + "','" +
+        perCostHuman + "','" + perCostEtc + "','" + perCostTotal + "');"
     );
+
+  
 });
 
 app.get('/list', function (요청, 응답) {
